dc5329809229a2ea90c8254302bbf0e2dde6c92b
Конпаку <sputnik-999-mel@mail.ru>
2024-02-08 15:18:18 +0500

    Answer

---
+++ Answer.java
@@ -0,0 +1,17 @@
+public class Answer {
+    private int cowCounter;
+    private int bullCounter;
+    private Integer tryCount;
+
+    public Answer(int cowCounter, int bullCounter, Integer tryCount) {
+        this.cowCounter = cowCounter;
+        this.bullCounter = bullCounter;
+        this.tryCount = tryCount;
+    }
+
+    @Override
+    public String toString() {
+        return "Коров " + cowCounter + ", быков " + bullCounter;
+
+    }
+}
